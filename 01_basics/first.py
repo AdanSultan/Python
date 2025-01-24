2f9bffb82a8809a6ee7e45c2a08d1619c5d193a9
Adan Sultan <adansultan794@gmail.com>
2025-01-24 20:30:06 +0500

    Start Python setup and focus on improving coding skills

---
+++ 01_basics/first.py
@@ -0,0 +1,8 @@
+from starting import string 
+
+# Jo methods pichli file ma def kiya inha import kr ka
+# likhna hota functions ka name likhna hota 
+
+# Name("Adan Sultan ,, @")
+
+string("Hello world...")
